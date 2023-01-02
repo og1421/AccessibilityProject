a517e1df2a7f55af4af7b10073c5fb3abfe2705d
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-02 20:11:44 -0300

    Initial Commit

---
+++ AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Orlando Moraes Martins on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
